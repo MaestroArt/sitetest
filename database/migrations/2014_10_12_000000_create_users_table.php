e6b517a4b74cbdf654ac012a44c4524d1ef52ca5
maestro\Artem <artem.lucky@gmail.com>
2018-11-02 13:58:49 +0200

    On old MySql shown errors.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('surname');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->date('birthday');
